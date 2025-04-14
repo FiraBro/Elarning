962e6d1fa5cc714669ccba54d8e2911b021bcb18
Firagos <jemalfiragos@gmail.com>
2025-04-14 17:52:47 +0300

    i modified the file

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-const courseRoutes = require('./routes/courseRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const courseRoutes = require("./routes/courseRoutes");
 // const paymentRoutes = require('./routes/paymentRoutes');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/courses', courseRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/courses", courseRoutes);
 // app.use('/api/payments', paymentRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
