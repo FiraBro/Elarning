80bad6a53d1c9f190f5336bb53ea0dab434904d1
Firagos <jemalfiragos@gmail.com>
2025-04-14 12:06:18 +0300

    i modified

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const { register, login } = require('../controllers/authController');
-const { validateRegister, validateLogin } = require('../utils/validation');
+const express = require("express");
+const {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController");
+const { validateRegister, validateLogin } = require("../utils/validation");
 
 const router = express.Router();
 
-router.post('/register', validateRegister, register);
-router.post('/login', validateLogin, login);
+router.post("/register", validateRegister, register);
+router.post("/login", validateLogin, login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
