68927c5cf435485cbab331dee9a73f06d5d47a07
Firagos <jemalfiragos@gmail.com>
2025-04-14 08:01:38 +0300

    i modefied it

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const { register, login } = require('../controllers/authController');
+const { validateRegister, validateLogin } = require('../utils/validation');
 
-router.post('/register', register);
-router.post('/login', login);
+const router = express.Router();
+
+router.post('/register', validateRegister, register);
+router.post('/login', validateLogin, login);
 
 module.exports = router;
