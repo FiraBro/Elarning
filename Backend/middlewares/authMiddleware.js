e234b69f5059890372f515e09734d39a6b6280f6
Firagos <jemalfiragos@gmail.com>
2025-04-14 08:08:01 +0300

    i create authMiddleware file

---
+++ Backend/middlewares/authMiddleware.js
@@ -0,0 +1,16 @@
+const { verifyToken } = require('../config/jwt');
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) return res.status(401).json({ message: 'No token provided' });
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
