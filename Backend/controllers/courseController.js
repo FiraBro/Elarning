90d10a4be6acc9d63ac24c76db6ae2eb487f4662
Firagos <jemalfiragos@gmail.com>
2025-04-14 07:57:11 +0300

    í modified it

---
+++ Backend/controllers/courseController.js
@@ -0,0 +1,28 @@
+const Course = require("../models/Course");
+
+const createCourse = async (req, res) => {
+  const { title, description, price } = req.body;
+  try {
+    const course = new Course({
+      title,
+      description,
+      price,
+      instructor: req.user.id,
+    });
+    await course.save();
+    res.status(201).json(course);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const getCourses = async (req, res) => {
+  try {
+    const courses = await Course.find().populate("instructor", "name");
+    res.json(courses);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { createCourse, getCourses };
