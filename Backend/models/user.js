0dd9273681627806ab566709ee222958f8f50937
Firagos <jemalfiragos@gmail.com>
2025-04-15 17:32:44 +0300

    i modified the file

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
     passwordResetExpires: Date,
     role: {
       type: String,
-      enum: ["student", "instructor", "admin"],
+      enum: ["user", "instructor", "admin"],
       default: "student",
     },
     avatar: {
