8f7950ecb6a97552d4198458b22a685dc16b0272
Firagos <jemalfiragos@gmail.com>
2025-04-17 13:39:07 +0300

    i create post request in the user routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,4 +2,6 @@ const express = require("express");
 const userRouter = express.Router();
 const userController = require("../controllers/userController");
 userRouter.route("/").get(userController.getAllUser);
+userRouter.route("/").post(userController.createUser);
+
 module.exports = userRouter;
