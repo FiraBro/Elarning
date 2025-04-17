bfc774fb7496bd1d1085712d3709ad630bde54c7
Firagos <jemalfiragos@gmail.com>
2025-04-17 18:00:04 +0300

    i fix the bug

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const { generateToken } = require("../config/jwt");
 const { sendEmail } = require("../services/emailService");
 const crypto = require("crypto");
