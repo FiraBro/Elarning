41ab38924f8000eec1a39ac2bc1b810be6312c8c
Firagos <jemalfiragos@gmail.com>
2025-04-17 11:02:46 +0300

    i create getuser file

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,9 +3,21 @@ const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
 exports.getAllUser = catchAsync(async (req, res, next) => {
-  const user = await User.find();
-  if (!user) {
+  const users = await User.find();
+  if (!users) {
     return next(new AppError("No user is found please rty again later!", 400));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      users,
+    },
+  });
+});
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return next(new AppError("No User is Found with this ID.", 400));
   }
   res.status(200).json({
     status: "success",
@@ -14,4 +26,3 @@ exports.getAllUser = catchAsync(async (req, res, next) => {
     },
   });
 });
-
