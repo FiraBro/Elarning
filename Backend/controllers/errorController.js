be68e65531d4a8d9f60bdbd03c0f432f2f95630d
Firagos <jemalfiragos@gmail.com>
2025-04-17 18:00:49 +0300

    i create global error controller

---
+++ Backend/controllers/errorController.js
@@ -0,0 +1,14 @@
+// Global error handling middleware
+const globalErrorHandler = (err, req, res, next) => {
+  console.error("ERROR 💥:", err);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message || "Something went wrong!",
+  });
+};
+
+module.exports = globalErrorHandler;
