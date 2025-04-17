644f1d059459d443a9ad4cdb18ae4498be9edbc7
Firagos <jemalfiragos@gmail.com>
2025-04-17 23:09:51 +0300

    i create three function in user controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,6 +2,13 @@ const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
+const filterObj = (obj, allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 exports.getAllUser = catchAsync(async (req, res, next) => {
   const users = await User.find();
   if (!users) {
@@ -9,7 +16,7 @@ exports.getAllUser = catchAsync(async (req, res, next) => {
   }
   res.status(200).json({
     status: "success",
-    length:users.length,
+    length: users.length,
     data: {
       users,
     },
@@ -27,12 +34,53 @@ exports.getUser = catchAsync(async (req, res, next) => {
     },
   });
 });
-exports.getMe = catchAsync(async(req,res,next)=>{
-
-})
+exports.getMe = catchAsync(async (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+});
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password) {
+    return next(
+      new AppError(
+        "This not the right place for updating password,please use reset-password instead!",
+        400
+      )
+    );
+  }
+  const filteredFields = filterObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    filteredFields,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).json({
+    status: "success",
+    user: updatedUser,
+  });
+});
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndDelete(req.user.id, { active: false });
+  res.status(200).json({
+    status: "success",
+    data: null,
+  });
+});
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  const doc = await User.findByIdAndDelete(req.params.id);
+  if (!doc) {
+    return next(new AppError("No document is found with this ID!", 400));
+  }
+  res.status(200).json({
+    status: "success",
+    message: "User with this ID is successfully deleted",
+  });
+});
 exports.createUser = (req, res) => {
-    res.status(500).json({
-      status: 'fail',
-      message: 'This route is not defined! Please use /register instead'
-    });
-  };
+  res.status(500).json({
+    status: "fail",
+    message: "This route is not defined! Please use /register instead",
+  });
+};
