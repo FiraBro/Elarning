b56f87ffc0284c292b9eed7b81809e1b4dc1d0dc
Firagos <jemalfiragos@gmail.com>
2025-04-19 22:55:28 +0300

    i modified

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -4,16 +4,6 @@ const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-// const multerStorage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "upload/userImage");
-//   },
-//   filename: (req, file, cb) => {
-//     const ext = file.mimetype.split("/")[1];
-//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-//   },
-// });
-
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
@@ -83,31 +73,6 @@ exports.getMe = catchAsync(async (req, res, next) => {
   next();
 });
 
-// exports.updateMe = catchAsync(async (req, res, next) => {
-//   if (req.body.password) {
-//     return next(
-//       new AppError(
-//         "This not the right place for updating password,please use reset-password instead!",
-//         400
-//       )
-//     );
-//   }
-//   const filteredFields = filterObj(req.body, "name", "email");
-//   if (req.file) filteredFields.photo = req.file.filename;
-//   const updatedUser = await User.findByIdAndUpdate(
-//     req.user.id,
-//     filteredFields,
-//     {
-//       new: true,
-//       runValidators: true,
-//     }
-//   );
-//   res.status(200).json({
-//     status: "success",
-//     user: updatedUser,
-//   });
-// });
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password) {
     return next(
@@ -164,40 +129,4 @@ exports.createUser = (req, res) => {
   });
 };
 
-// const filterObj = (obj, allowedFields) => {
-//   const newObj = {};
-//   Object.keys(obj).forEach((el) => {
-//     if (allowedFields.includes(el)) newObj[el] = obj[el];
-//   });
-//   return newObj;
-// };
 
-// exports.updateMe = catchAsync(async (req, res, next) => {
-//   if (req.body.password) {
-//     return next(
-//       new AppError(
-//         "This not the right place for updating password, please use reset-password instead!",
-//         400
-//       )
-//     );
-//   }
-
-//   // Pass allowed fields as an array
-//   const filteredFields = filterObj(req.body, ["name", "email"]);
-
-//   if (req.file) filteredFields.photo = req.file.filename;
-
-//   const updatedUser = await User.findByIdAndUpdate(
-//     req.user.id,
-//     filteredFields,
-//     {
-//       new: true,
-//       runValidators: true,
-//     }
-//   );
-
-//   res.status(200).json({
-//     status: "success",
-//     user: updatedUser,
-//   });
-// });
