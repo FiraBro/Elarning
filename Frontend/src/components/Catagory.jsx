f594f13d986175fd836c003600e0ffde4496d808
Firagos <jemalfiragos@gmail.com>
2025-04-21 21:05:19 +0300

    i create an catagory section

---
+++ Frontend/src/components/Catagory.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./Catagory.module.css";
+export default function Catagory() {
+  return (
+    <div className={style.ctagory}>
+      <div>Programming & Software Development</div>
+      <div>Video Editing</div>
+      <div>Graphic Design</div>
+      <div>Communication & Soft Skills</div>
+      <div>Digital Marketing & SEO</div>
+      <div>Frontend Developer</div>
+      <div>Backend Developer</div>
+    </div>
+  );
+}
