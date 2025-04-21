1e4961ee6b322baa5d3b81020ad1c2bea44d285a
Firagos <jemalfiragos@gmail.com>
2025-04-21 13:07:29 +0300

    i finally finished the navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { FaBookOpen } from "react-icons/fa";
 import style from "./Navbar.module.css";
+
+// Logo component displaying the open book icon
+function Logo() {
+  return <FaBookOpen size={48} color="green" cursor='pointer' />;
+}
+
 export default function Navbar() {
   return (
     <div className={style.navbar}>
-      <div className={style.left}>Logo</div>
+      <div className={style.left}>
+        <Logo />
+      </div>
       <div className={style.right}>
         <button className={style.myCourse}>MyCourse</button>
         <button className={style.authentication}>Login</button>
