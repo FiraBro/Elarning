872e09970ebe558cc2fa7443064d29e1e7b1ed93
Firagos <jemalfiragos@gmail.com>
2025-04-22 22:46:47 +0300

    i create featured instructor

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -7,6 +7,8 @@ import BenefitsSection from "../components/BenefitsSection";
 import HowItWorksSection from "../components/HowItWorksSection";
 import CertificationsOutcomesSection from "../components/CertificationsOutcomesSection";
 import NewsletterSignup from "../components/NewsletterSignup";
+import Footer from "../components/Footer";
+import FeaturedInstructors from "../components/FeaturedInstructors";
 
 export default function HomePage() {
   return (
@@ -18,7 +20,9 @@ export default function HomePage() {
       <BenefitsSection />
       <HowItWorksSection />
       <CertificationsOutcomesSection />
+      <FeaturedInstructors />
       <NewsletterSignup />
+      <Footer />
     </div>
   );
 }
