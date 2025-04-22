a50711842564bfc70ba6252f28a6d94398a7fc43
Firagos <jemalfiragos@gmail.com>
2025-04-22 18:09:27 +0300

    í modified it

--- Frontend/src/components/Overview.jsx
+++ Frontend/src/components/Overview.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import style from "./overview.module.css";
-// import demo from "../../public/demo.jpg";
-export default function Overview() {
-  return (
-    <div className={style.overview}>
-      <div className="">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa numquam
-          maxime, iusto exercitationem dicta culpa minus est voluptatem
-          temporibus autem. Numquam et excepturi eveniet quidem laboriosam amet,
-          libero tempora culpa?
-        </p>
-      </div>
-      <div className="">
-        <img src='/demo.jpg' alt="demo" width={400} height={200} />
+import React from 'react';
+import styles from './Overview.module.css';
+
+const Overview = () => (
+  <header className={styles.hero}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Unlock Your Potential with <span className={styles.highlight}>E-Learning</span></h1>
+      <p className={styles.subtitle}>
+        Join thousands of learners and start mastering new skills, anytime, anywhere.
+      </p>
+      <div className={styles.ctaGroup}>
+        <a href="#courses" className={styles.ctaPrimary}>Browse Courses</a>
+        <a href="#signup" className={styles.ctaSecondary}>Sign Up Free</a>
       </div>
     </div>
-  );
-}
+    <img
+      src="https://img.freepik.com/free-vector/online-learning-concept-illustration_114360-4764.jpg"
+      alt="E-learning illustration"
+      className={styles.heroImage}
+    />
+  </header>
+);
+
+export default Overview;
