b40ad134d835900eeaed2c29c9415ec2f3cb268d
Firagos <jemalfiragos@gmail.com>
2025-04-22 18:13:41 +0300

    i add benefit component to it

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Overview from "../components/overview";
+import Catagory from "../components/catagory";
+import FeaturedCourse from "../components/FeaturedCourse";
+import BenefitsSection from "../components/BenefitsSection";
 
 export default function HomePage() {
   return (
     <div>
       <Navbar />
+      <Overview />
+      <Catagory />
+      <FeaturedCourse />
+      <BenefitsSection />
     </div>
   );
 }
