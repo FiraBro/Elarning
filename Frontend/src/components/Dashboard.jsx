1d7212f18efd8ae0537e72a799162b0ada1a7b00
Firagos <jemalfiragos@gmail.com>
2025-04-23 17:44:18 +0300

    allmost finish

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import style from './Dashboard.module.css'
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className={style.dashboard}>
+      <div className={style.head}>
+        <h1>Dashboard</h1>
+      </div>
+      <div className={style.revenueTotal}>
+        <div className={style.total}>Total Student:5</div>
+        <div className={style.revenue}>Total Revenue:100</div>
+      </div>
+      <div className={style.studentList}>
+        <div className={style.studentListHeadOne}>Student List</div>
+        <div className={style.studentListHeadTwo}>
+          <div className="">Course Name</div>
+          <div className="">Student Name</div>
+          <div className="">Student Email</div>
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
   )
 }
