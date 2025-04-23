03ce035ab9ccd8a97f411d2ce3943ad1dcf144e0
Firagos <jemalfiragos@gmail.com>
2025-04-23 07:24:48 +0300

    i finaly add multple page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";  // <-- Import Routes and Route
+
 import HomePage from "./pages/HomePage";
+import MycoursePage from "./pages/MycoursePage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
     <div className="app">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/mycourses" element={<MycoursePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
