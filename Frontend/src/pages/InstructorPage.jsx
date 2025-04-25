2ffe4c16d1cf759b196d94234215f9d4e3f4aa9f
Firagos <jemalfiragos@gmail.com>
2025-04-25 09:46:11 +0300

    i modified the content

---
+++ Frontend/src/pages/InstructorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import Applayout from "../iu/AdminLayout";
+import style from "./InstructorPage.module.css";
+import AdminLayout from "../iu/AdminLayout";
+export default function InstructorPage() {
+  return (
+    <div className={style.instructor}>
+      <AdminLayout />
+    </div>
+  );
+}
