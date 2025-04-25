72266808102fd592d48f9004b07f00b2991e736a
Firagos <jemalfiragos@gmail.com>
2025-04-25 10:48:13 +0300

    i create catagory

--- Frontend/src/components/Catagory.jsx
+++ Frontend/src/components/Catagory.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import style from "./Catagory.module.css";
+import React from 'react'
+import style from './catagory.module.css'
 export default function Catagory() {
   return (
-    <div className={style.ctagory}>
-      <div>Programming & Software Development</div>
-      <div>Video Editing</div>
-      <div>Graphic Design</div>
-      <div>Communication & Soft Skills</div>
-      <div>Digital Marketing & SEO</div>
-      <div>Frontend Developer</div>
-      <div>Backend Developer</div>
+    <div className={style.catagory}>
+      <p>Discover variety of courses and start learning!</p>
+      <div className={style.browseCourse}>
+        <button className={style.browseBtn}>Browse Course</button>
+        <button className={style.startLearning}>Start Learning</button>
+      </div>
     </div>
-  );
+  )
 }
