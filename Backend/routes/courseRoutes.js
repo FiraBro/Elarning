4cdef86cec9f37cf7e69288615aa0bf46221fa35
Firagos <jemalfiragos@gmail.com>
2025-04-26 07:43:52 +0300

    i add two more routes to it

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const { createCourse, getCourses } = require("../controllers/courseController");
+// const { createCourse, getCourses } = require("../controllers/courseController");
+const courseController = require("../controllers/courseController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const adminMiddleware = require("../middlewares/adminMiddleware");
+const authController = require('../controllers/authController')
 
 const router = express.Router();
 
-router.post("/", authMiddleware,adminMiddleware, createCourse);
-router.get("/", getCourses);
+router.post(
+  "/",
+  authMiddleware,
+  adminMiddleware,
+  courseController.createCourse
+);
+router.get("/", courseController.getCourses);
+router.get("/metrics", courseController.getCourseMetrics);
+router.post(
+  "/:courseId/enroll",
+  authController.protect,
+  courseController.trackEnrollment,
+//   paymentController.handlePayment
+);
 
 module.exports = router;
