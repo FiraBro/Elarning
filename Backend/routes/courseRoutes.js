4851f7d01c5aba4cbe7aa9ca0195beb3bc2672dd
Firagos <jemalfiragos@gmail.com>
2025-04-26 11:47:38 +0300

    i add one more route

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -3,23 +3,30 @@ const express = require("express");
 const courseController = require("../controllers/courseController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const adminMiddleware = require("../middlewares/adminMiddleware");
-const authController = require('../controllers/authController')
+const authController = require("../controllers/authController");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" }); // Specify the upload directory
 
 const router = express.Router();
-
+// Route definition with multer middleware
 router.post(
   "/",
   authMiddleware,
   adminMiddleware,
+  upload.fields([
+    { name: "video", maxCount: 1 },
+    { name: "banner", maxCount: 1 },
+  ]),
   courseController.createCourse
 );
+
 router.get("/", courseController.getCourses);
 router.get("/metrics", courseController.getCourseMetrics);
 router.post(
   "/:courseId/enroll",
   authController.protect,
-  courseController.trackEnrollment,
-//   paymentController.handlePayment
+  courseController.trackEnrollment
+  //   paymentController.handlePayment
 );
 
 module.exports = router;
