300d473bf12be4309cc31e9ea32829f70080930a
Firagos <jemalfiragos@gmail.com>
2025-04-29 11:47:52 +0300

    i f modified

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MycoursePage from "./pages/MycoursePage";
-import Dashboard from "./components/Dashboard";
 import CourseController from "./components/CourseController";
 import InstructorPage from "./pages/InstructorPage";
 import SingupPage from "./pages/SingupPage";
 import LoginPage from "./pages/LoginPage";
-import Profile from "./components/Profile";
+import Profile from "./components/Profile/Profile";
 import AdminLayout from "./iu/AdminLayout";
-import UserControl from "./components/UserController";
+import UserControl from "./components/UserController/UserController";
 import CourseLessons from "./components/CourseLesson/CourseLessons";
+import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -47,7 +47,7 @@ export default function App() {
       children: [
         {
           path: "admin/dashboard",
-          element: <Dashboard />,
+          element: <AdminDashboard />,
         },
         {
           path: "admin/courses",
