cf4c06128d8161dede9cc4f446bb3eee2d94d5cf
Firagos <jemalfiragos@gmail.com>
2025-05-03 21:14:16 +0300

    i made change to it

--- Backend/utils/validation.js
+++ Backend/utils/validation.js
@@ -1,23 +1,25 @@
 const validateRegister = (req, res, next) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      return res.status(400).json({ message: 'All fields are required' });
-    }
-    if (!/\S+@\S+\.\S+/.test(email)) {
-      return res.status(400).json({ message: 'Invalid email' });
-    }
-    if (password.length < 6) {
-      return res.status(400).json({ message: 'Password must be at least 6 characters' });
-    }
-    next();
-  };
-  
-  const validateLogin = (req, res, next) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({ message: 'All fields are required' });
-    }
-    next();
-  };
-  
-  module.exports = { validateRegister, validateLogin };
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  if (!/\S+@\S+\.\S+/.test(email)) {
+    return res.status(400).json({ message: "Invalid email" });
+  }
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters" });
+  }
+  next();
+};
+
+const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  next();
+};
+
+module.exports = { validateRegister, validateLogin };
