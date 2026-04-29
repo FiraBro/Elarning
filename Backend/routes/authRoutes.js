f73231d00d8ad0089efb4e82759cb517b04d37c3
FiraBro <jemalfiragos@gmail.com>
2026-04-29 13:20:25 +0300

    fix the route

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,14 +5,14 @@ const {
   forgotPassword,
   resetPassword,
 } = require("../controllers/authController");
-const { validateRegister, validateLogin } = require("../utils/validation");
-const { authLimiter } = require("../middlewares/rateLimit");
+// const { validateRegister, validateLogin } = require("../utils/validation");
+// const { authLimiter } = require("../middlewares/rateLimit");
 
 const router = express.Router();
 
-router.post("/register", authLimiter, validateRegister, register);
-router.post("/login", authLimiter, validateLogin, login);
-router.post("/forgot-password", authLimiter, forgotPassword);
-router.patch("/reset-password/:token", authLimiter, resetPassword);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.patch("/reset-password/:token", resetPassword);
 
 module.exports = router;
